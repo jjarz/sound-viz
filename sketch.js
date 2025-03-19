43070a57c5b28f48ff593df64ccce9c3762580f4
Julie Jarzemsky <jjarzem@gmail.com>
2025-03-19 11:37:48 -0600

    change style

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let mic, fft;
 let started = false;
+let particles = [];
+const numParticles = 100;
 
 function setup() {
     const canvas = createCanvas(windowWidth * 0.8, windowHeight * 0.6);
@@ -12,48 +14,105 @@ function setup() {
     // Connect mic to FFT for analysis
     fft.setInput(mic);
     
-    // Setup button handler
-    const startButton = select('#startButton');
-    startButton.mousePressed(toggleAudio);
+    // Create particles
+    for (let i = 0; i < numParticles; i++) {
+        particles.push(new Particle());
+    }
     
     // Initial styling
     colorMode(HSB);
     noFill();
+    
+    // Setup button handler
+    const startButton = select('#startButton');
+    startButton.mousePressed(toggleAudio);
+}
+
+class Particle {
+    constructor() {
+        this.reset();
+        this.angle = random(TWO_PI);
+        this.radius = random(50, 150);
+        this.speed = random(0.02, 0.05);
+    }
+    
+    reset() {
+        this.x = width / 2;
+        this.y = height / 2;
+        this.size = random(2, 5);
+        this.alpha = 255;
+        this.hue = random(160, 280); // Blue to purple range
+    }
+    
+    update(energy) {
+        this.angle += this.speed * energy;
+        this.radius += energy * 0.5;
+        
+        if (this.radius > max(width, height)) {
+            this.reset();
+        }
+        
+        this.x = width/2 + cos(this.angle) * this.radius;
+        this.y = height/2 + sin(this.angle) * this.radius;
+    }
+    
+    draw() {
+        stroke(this.hue, 100, 100, this.alpha);
+        strokeWeight(this.size);
+        point(this.x, this.y);
+    }
 }
 
 function draw() {
-    background(0);
+    background(0, 0, 0, 20); // Slight trail effect
     
     if (started) {
-        // Analyze the frequency spectrum
+        // Analyze the audio
         let spectrum = fft.analyze();
+        let waveform = fft.waveform();
         
-        // Draw the spectrum
-        beginShape();
+        // Calculate average energy
+        let energy = 0;
         for (let i = 0; i < spectrum.length; i++) {
-            let x = map(i, 0, spectrum.length, 0, width);
-            let h = map(spectrum[i], 0, 255, height, 0);
-            
-            // Calculate color based on frequency
-            let hue = map(i, 0, spectrum.length, 0, 360);
-            stroke(hue, 100, 100);
-            strokeWeight(2);
-            
-            vertex(x, h);
+            energy += spectrum[i];
         }
-        endShape();
+        energy = energy / spectrum.length / 255;
+        
+        // Update and draw particles
+        push();
+        blendMode(ADD);
+        particles.forEach(p => {
+            p.update(energy);
+            p.draw();
+        });
+        pop();
+        
+        // Draw center circle
+        let radius = 100 + energy * 50;
+        noFill();
+        strokeWeight(2);
+        for (let i = 0; i < 3; i++) {
+            let alpha = map(i, 0, 2, 255, 50);
+            stroke(200, 100, 100, alpha);
+            circle(width/2, height/2, radius + i * 10);
+        }
         
         // Draw waveform
-        let waveform = fft.waveform();
+        push();
         beginShape();
-        stroke(255);
-        strokeWeight(1);
+        noFill();
+        strokeWeight(2);
+        stroke(200, 100, 100, 100);
         for (let i = 0; i < waveform.length; i++) {
-            let x = map(i, 0, waveform.length, 0, width);
-            let y = map(waveform[i], -1, 1, height/2 - 50, height/2 + 50);
+            let angle = map(i, 0, waveform.length, 0, TWO_PI);
+            let r = map(waveform[i], -1, 1, radius - 20, radius + 20);
+            let x = width/2 + cos(angle) * r;
+            let y = height/2 + sin(angle) * r;
             vertex(x, y);
         }
-        endShape();
+        endShape(CLOSE);
+        pop();
+        
     } else {
         // Show waiting message
         textAlign(CENTER, CENTER);
@@ -64,15 +123,13 @@ function draw() {
     }
 }
 
-function toggleAudio() {
+window.toggleAudio = function() {
     if (!started) {
-        // Start audio input
         userStartAudio();
         mic.start();
         started = true;
         select('#startButton').html('Stop Microphone');
     } else {
-        // Stop audio input
         mic.stop();
         started = false;
         select('#startButton').html('Start Microphone');
